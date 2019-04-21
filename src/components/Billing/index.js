02cc40ef17b84521e5050bb0a3b38d17494159e0
Dennise Ortega <dennise.i.ortega@gmail.com>
2019-04-21 10:54:50 -0700

    add class to components

--- src/components/Billing/index.js
+++ src/components/Billing/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 
+class Billing extends Component {
+  render() {
+    return (
+        <div>
+          <h2>Billing</h2>
+        </div>
+    );
+  }
+}
 
+export default Billing;
 
 
 
--- src/components/OrderComplete/index.js
+++ src/components/OrderComplete/index.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
 
+class OrderComplete extends Component {
+  render() {
+    return (
+        <div>
+          <h2>OrderComplete</h2>
+        </div>
+    );
+  }
+}
+
+export default OrderComplete;
 
 
 
